8d36802f71995580f05aeb75a10dff3ee119b5f9
Arnaud Goguelin <arnaud.goguelin@gmail.com>
2023-09-05 13:35:22 +0200

    modification du chiffrement et hachage des identifiants et mots de passe pour faciliter et simplifier le controller d'authentiufication "login"

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,33 +1,40 @@
 const User = require('../models/User');
 const JWT = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const secureDatas = require('../utils/secureDatas');
 
 exports.signup = async (req, res) => {
 	try{
+		if(!req.body.email || !req.body.password){
+			return res.status(400).send({
+				message: 'Email et mot de passe requis'
+			});
+		}
+
+		const hashedPassword = await secureDatas.hashData(req.body.password);
+		const encryptedEmail = secureDatas.encryptData(req.body.email);
+
 		const user = {
-			email: req.body.email,
-			password: req.body.password,
-			iv: req.body.iv
+			email: encryptedEmail,
+			password: hashedPassword
 		};
 		await User.create(user);
 		return res.status(201).json({ message: 'Utilisateur créé' });
 	} catch (error) {
-		return res.status(500).json({ message: error });
+		return res.status(500).json({ error });
 	}
 };
 
 exports.login = async (req, res) => {
 	try {
-		let userToFind = null;
-		for (let user of req.body.decryptedDatas) { if (user.email === req.body.email) {
-			userToFind = user;
-			break;
-		}}
+		const emailToFind = secureDatas.encryptData(req.body.email);
+
+		const userToFind = await User.findOne({ email: emailToFind});
 
 		if (userToFind === null) {
 			res.status(401).json({ message: 'Identifiant ou mot de passe incorrect(s)' });
 		}
-		
+
 		const hasComparison = await	bcrypt.compare(req.body.password, userToFind.password);
 
 		if (hasComparison === false) {
@@ -52,29 +59,31 @@ exports.login = async (req, res) => {
 	}
 };
 
-// exports.login = (req, res) => {
-// 	User.findOne({ email: req.body.email })
-// 		.then(user => {
-// 			if (user === null) {
-// 				res.status(401).json({ message: 'Identifiant ou mot de passe incorrect(s)' });
-// 			} else {
-// 				bcrypt.compare(req.body.password, user.password)
-// 					.then(valid => {
-// 						if (!valid) {
-// 							res.status(401).json({ message: 'Identifiant ou mot de passe incorrect(s)' });
-// 						} else {
-// 							res.status(200).json({
-// 								userId: user._id,
-// 								token: JWT.sign(
-// 									{ userId: user._id },
-// 									process.env.JWTKEY,
-// 									{ expiresIn: '24h' }
-// 								)
-// 							});
-// 						}
-// 					})
-// 					.catch(error => res.status(500).json({ error }));
-// 			}
-// 		})
-// 		.catch(error => res.status(500).json({ error }));
-// };
+// let userToFind = null;
+// for (let user of req.body.decryptedDatas) { if (user.email === req.body.email) {
+// 	userToFind = user;
+// 	break;
+// }}
+
+// if (userToFind === null) {
+// 	res.status(401).json({ message: 'Identifiant ou mot de passe incorrect(s)' });
+// }
+		
+// const hasComparison = await	bcrypt.compare(req.body.password, userToFind.password);
+
+// if (hasComparison === false) {
+// 	res.status(401).json({ message: 'Identifiant ou mot de passe incorrect(s)' });
+// } else {
+// 	try {
+// 		res.status(200).json({
+// 			userId: userToFind._id,
+// 			token: JWT.sign(
+// 				{ userId: userToFind._id },
+// 				process.env.JWTKEY,
+// 				{ expiresIn: '24h' }
+// 			)
+// 		});
+// 	} catch (error) {
+// 		error => res.status(500).json({ error });
+// 	}
+// }
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,12 +3,11 @@ const router = express.Router();
 
 const ERL = require('../middlewares/expressRateLimitMiddleware');
 const PWV = require('../middlewares/passwordValidatorMiddleware');
-const crypto = require('../middlewares/nodeCryptoMiddleware');
 
 const authController = require('../controllers/authController');
 
-router.post('/signup', ERL.createAccountLimiter, PWV.validateLogins, crypto.secureDatas, authController.signup);
+router.post('/signup', ERL.createAccountLimiter, PWV.validateLogins, authController.signup);
 
-router.post('/login', ERL.limiter, crypto.decryptData, authController.login);
+router.post('/login', ERL.limiter, authController.login);
 
 module.exports = router;
